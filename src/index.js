ff36eb3ba799ab170eb6007171631129d15facf4
Arslan Shakoor <arslantechielive@gmail.com>
2017-08-04 22:56:44 -0400

    add switch component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
  
 import promise from 'redux-promise';
 
@@ -19,8 +19,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        <Route path='/new' component={PostNew}/>
+        <Switch>
+          <Route path='/new' component={PostNew}/>
+          <Route path="/" component={PostsIndex} />
+        </Switch>
 	    </div>
     </BrowserRouter>
   </Provider>
