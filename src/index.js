4a7e22592757d2b3ee10bbcf4081b26b1676004b
Arslan Shakoor <arslantechielive@gmail.com>
2017-08-06 00:54:20 -0400

    recieve new post as show

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex  from './components/posts_index';
 import PostNew from './components/post_new';
+import PostShow from './components/post_show'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -22,7 +23,8 @@ ReactDOM.render(
 
         <Switch>
           <Route path='/new' component={PostNew}/>
-          <Route path="/" component={PostsIndex} />
+          <Route path='/posts/:id' component={PostShow}/>
+          <Route path="/" component={PostsIndex} /> 
         </Switch>
 	    </div>
     </BrowserRouter>
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,12 +1,27 @@
 import _ from  'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 
 export default function(state = {}, action){
 	switch (action.type){ 
 		case FETCH_POSTS:
-		//with lodash  we implement map for iteration and the function below we are making key value pairs
-		   return _.mapKeys(action.payload.data, 'id');
+		   // ....state get all the data which we already had..
+		   
+
+              //es5  is below
+           //   const post  = action.payload.data;
+		   // const newState = {...state,}
+		   // newState[post.id]= post;
+		   // return newState;
+
+		      // es6 is below
+		    return {...state, [action.payload.data.id]: action.payload.data}  
+
+
+
+		case FETCH_POST:
+		  //with lodash  we implement map for iteration and the function below we are making key value pairs
+		  return _.mapKeys(action.payload.data, 'id'); 
 		default: 
 		  return state;
     }
